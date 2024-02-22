46b73250c0fd55daf5b79a6733c013ddb4962bf5
Sh1em <shleg.egor2004@mail.ru>
2024-02-22 18:06:23 +0300

    Base

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"command-line-argumentsD:\\Codes\\Go\\Microservicw\\todo_by_hand\\server.go"
 	"log"
-	"todo"
+	todo "todo_app"
 	"todo_app/pkg/handler"
+	"todo_app/pkg/repository"
+	"todo_app/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error uncured initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error uncured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+
 }
---
+++ pkg/service/service.go
@@ -0,0 +1,20 @@
+package service
+
+import "todo_app/pkg/repository"
+
+type Authorization interface {
+}
+type TodoList interface {
+}
+type TodoItem interface {
+}
+
+type Service interface {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(repos *repository.Repository) *Service {
+	return &Service{}
+}
